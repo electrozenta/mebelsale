a4fcc505111099eca82c931c50d614a6a8f264a1
lorsabyan <lorsabyan@gmail.com>
2014-08-05 08:47:22 +0400

    some style changes

--- base.php
+++ base.php
@@ -12,18 +12,20 @@
     get_template_part('templates/header');
   ?>
 
-  <div class="wrap container" role="document">
-    <div class="content row">
-      <main class="main <?php echo roots_main_class(); ?>" role="main">
-        <?php include roots_template_path(); ?>
-      </main><!-- /.main -->
-      <?php if (roots_display_sidebar()) : ?>
-        <aside class="sidebar <?php echo roots_sidebar_class(); ?>" role="complementary">
-          <?php include roots_sidebar_path(); ?>
-        </aside><!-- /.sidebar -->
-      <?php endif; ?>
-    </div><!-- /.content -->
-  </div><!-- /.wrap -->
+  <div class="wrap">
+      <div class="container" role="document">
+          <div class="content row">
+              <?php if (roots_display_sidebar()) : ?>
+                  <aside class="sidebar <?php echo roots_sidebar_class(); ?>" role="complementary">
+                      <?php include roots_sidebar_path(); ?>
+                  </aside><!-- /.sidebar -->
+              <?php endif; ?>
+              <main class="main <?php echo roots_main_class(); ?>" role="main">
+                  <?php include roots_template_path(); ?>
+              </main><!-- /.main -->
+          </div><!-- /.content -->
+      </div><!-- /.wrap -->
+  </div>
 
   <?php get_template_part('templates/footer'); ?>
 
