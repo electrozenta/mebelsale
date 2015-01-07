05c4146a47bc36e7023da1c0967bf7fb93171385
Aghasi Lorsabyan <lorsabyan@gmail.com>
2015-01-07 23:39:35 +0400

    optional  products selection field added to the catalog template

--- template-catalog.php
+++ template-catalog.php
@@ -25,10 +25,12 @@ Template Name: Каталог
         <?php
 
         foreach ($catalogues as $catalog) {
-            $url = wp_get_attachment_url( get_post_thumbnail_id($catalog->ID) );
+            $url = wp_get_attachment_url(get_post_thumbnail_id($catalog->ID));
             ?>
             <div class="col-xs-6 col-sm-4 col-lg-3">
-                <h2><a href="<?php echo get_page_link($catalog->ID); ?>" style="background-image:url(<?php echo $url; ?>);" class="mb-box"><?php echo $catalog->post_title; ?></a></h2>
+                <h2><a href="<?php echo get_page_link($catalog->ID); ?>"
+                       style="background-image:url(<?php echo $url; ?>);"
+                       class="mb-box"><?php echo $catalog->post_title; ?></a></h2>
             </div>
         <?php
         }
@@ -38,19 +40,20 @@ Template Name: Каталог
 </div>
 
 
-    <?php
-    $furniture = get_pages(array(
-        'child_of' => $post->ID,
-        'hierarchical' => 1,
-        'meta_key' => '_wp_page_template',
-        'meta_value' => 'template-furniture.php',
-        'sort_column' => 'menu_order',
-        'sort_order' => 'asc',
-    ));
+<?php
+$furniture = get_pages(array(
+    'child_of' => $post->ID,
+    'hierarchical' => 1,
+    'meta_key' => '_wp_page_template',
+    'meta_value' => 'template-furniture.php',
+    'sort_column' => 'menu_order',
+    'sort_order' => 'asc',
+));
 
-    ?>
-    <ul class="row mb-furniture">
-        <?php
+?>
+<ul class="row mb-furniture">
+    <?php
+    if (!CFS()->get('hide_products')) :
 
         foreach ($furniture as $item) {
 
@@ -67,21 +70,62 @@ Template Name: Каталог
             <li class="col-xs-6 col-sm-4 col-lg-3 text-center">
                 <div class="mb-furniture-item mb-box">
                     <a href="<?php echo $url; ?>">
-                        <?php echo get_the_post_thumbnail( $item->ID, 'furniture thumbnail', array(
+                        <?php echo get_the_post_thumbnail($item->ID, 'furniture thumbnail', array(
                             'class' => "img-responsive",
                             'alt' => $alt
                         )); ?>
                     </a>
+
                     <h3><a href="<?php echo get_page_link($item->ID); ?>"><?php echo $item->post_title; ?></a></h3>
-                    <p><?php echo "Цена: ".$price." руб."; ?></p>
+
+                    <p><?php echo "Цена: " . $price . " руб."; ?></p>
                     <a href="<?php echo get_page_link($item->ID); ?>" class="mb-details">подробнее...</a>
-                    <a href="#" class="mb-btn-buy" role="button" data-toggle="modal" data-target="#order" data-url="<?php echo $url; ?>">купить</a>
+                    <a href="#" class="mb-btn-buy" role="button" data-toggle="modal" data-target="#order"
+                       data-url="<?php echo $url; ?>">купить</a>
                 </div>
             </li>
         <?php
+
         }
 
+    endif;
+
+    $pages = CFS()->get('select_products');
+
+    foreach ($pages as $page_id) :
+        $item = get_post($page_id);
+
+        $post_thumbnail_id = get_post_thumbnail_id($item->ID);
+        $attachment_url = wp_get_attachment_url($post_thumbnail_id);
+        //$alt = get_post_meta($post_thumbnail_id, '_wp_attachment_image_alt', true);
+        $alt = get_post_meta($item->ID, _aioseop_title, true);
+
+        $url = get_page_link($item->ID);
+
+        $price = CFS()->get('furniture_price', $item->ID);
+
         ?>
-    </ul>
+        <li class="col-xs-6 col-sm-4 col-lg-3 text-center">
+            <div class="mb-furniture-item mb-box">
+                <a href="<?php echo $url; ?>">
+                    <?php echo get_the_post_thumbnail($item->ID, 'furniture thumbnail', array(
+                        'class' => "img-responsive",
+                        'alt' => $alt
+                    )); ?>
+                </a>
+
+                <h3><a href="<?php echo get_page_link($item->ID); ?>"><?php echo $item->post_title; ?></a></h3>
+
+                <p><?php echo "Цена: " . $price . " руб."; ?></p>
+                <a href="<?php echo get_page_link($item->ID); ?>" class="mb-details">подробнее...</a>
+                <a href="#" class="mb-btn-buy" role="button" data-toggle="modal" data-target="#order"
+                   data-url="<?php echo $url; ?>">купить</a>
+            </div>
+        </li>
+    <?php
+    endforeach;
+
+    ?>
+</ul>
 
 
